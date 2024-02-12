e62a31609bc1784331db75172a6194581e4c5b92
yamaniha2 <yamaniha2@gmail.com>
2024-02-12 10:46:16 +0900

    no image対応

--- examples/web/client/js/src/manual.js
+++ examples/web/client/js/src/manual.js
@@ -541,3 +541,17 @@ Manual.ultrasonicReset = function () {
 Manual.grayScaleReset = function () {
     $('.line_of_inspection_item_danger').hide();
 }
+
+Manual.grayScaleReset = function () {
+    $('.line_of_inspection_item_danger').hide();
+}
+
+$(document).ready(function(){
+	$('img.usb_video').error(function() {
+		$(this).attr({
+			src: 'image/no_image.png',
+			alt: 'NoImage',
+			class: ''
+		});
+	});
+});
